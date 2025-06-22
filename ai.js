0c8a91eee70c9950df171f0a00868403d22c9341
sathu-beep <schundru@umich.edu>
2025-06-21 20:33:29 -0700

    kdjaf

--- ai.js
+++ ai.js
@@ -15,6 +15,17 @@ export async function generateQuest(user, completedTitles) {
   It should be either indoor or outdoor depending on their setting: ${user.preference}.
   Keep it under 25 words and give it a title and 1-sentence description.
 
+  The quest needs to be specific to a certain location. Simply saying "Go to a restaurant and try a new dish" is not enough.
+  The quest should name a specific restaurant, cafe, park, etc that has good reviews and is a popular spot.
+
+  An example of a good quest:
+  "Go to the Philadelphia Museum of Art and see the "American Gothic" painting while listening to a new album for the first time."
+
+  An example of a bad quest:
+  Find hidden NYC-themed goodies in a department store.
+
+  The title should have the title of the location the activity takes place at in it.
+  
   Return ONLY valid JSON in this exact format:
   {
     "title": "...",
